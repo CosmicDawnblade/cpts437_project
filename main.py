5cc2c7e3295a5302d6761bbc0f3571d4d84ca401
Khoa Bui <khoa.bui@wsu.edu>
2022-04-27 01:43:35 -0700

    added results of matches as labels for the algorithm

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import sqlite3
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import Perceptron
 
+
+def matches_label(matches):
+    labels = pd.DataFrame()
+    labels.insert(0, 'match_api_id', matches['match_api_id'])
+    labels['result'] = np.where(matches['home_team_goal'] > matches['away_team_goal'], 'Win', 'Lose')
+    return labels
 
 def main():
+    # Parsing from sql file to panda's df's
     cnx = sqlite3.connect('database.sqlite')
     country_df = pd.read_sql_query("select * from Country", cnx)
     league_df = pd.read_sql_query("select * from League", cnx)
@@ -12,15 +21,11 @@ def main():
     team_df = pd.read_sql_query("select * from Team", cnx)
     team_att_df = pd.read_sql_query("select * from Team_Attributes", cnx)
 
+    # Create labels for matches
+    #print(match_df['id'])
+    labels = matches_label(match_df)
+    print(labels)
 
-
-    print(country_df)
-    print(league_df)
-    print(match_df)
-    print(player_df)
-    print(player_att_df)
-    print(team_df)
-    print(team_att_df)
 
 
 if __name__ == "__main__":
