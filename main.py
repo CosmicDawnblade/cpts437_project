f2039badbf8ea349c737bae6e29f63419616b3b9
CosmicDawnblade <39527908+CosmicDawnblade@users.noreply.github.com>
2022-04-27 15:05:25 -0700

    Finished cleaning/wrangling data
    Data is now ready to be inputted into the perceptron algorithm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 
 
 def main():
@@ -12,17 +13,32 @@ def main():
     team_df = pd.read_sql_query("select * from Team", cnx)
     team_att_df = pd.read_sql_query("select * from Team_Attributes", cnx)
 
-    
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
 
+    match_fil_df = match_df[['home_team_api_id', 'stage', 'home_team_goal', 'away_team_goal']]
 
+    X = team_att_df[['team_api_id', 'date', 'buildUpPlaySpeed', 'buildUpPlayDribbling', 'buildUpPlayPassing',
+                     'chanceCreationPassing', 'chanceCreationCrossing', 'chanceCreationShooting', 'defencePressure',
+                     'defenceAggression', 'defenceTeamWidth']]
+    # X = X.groupby(['team_api_id'])['date'].transform(max)
+    X = X.sort_values('date', ascending=False).drop_duplicates(['team_api_id'])
+    X = X.sort_values('team_api_id')
+    X = X.drop('date', axis=1)
+    X = pd.merge(match_fil_df, X, left_on='home_team_api_id', right_on='team_api_id')
+    X = X.drop(['home_team_api_id', 'team_api_id'], axis=1)
 
-    print(country_df)
-    print(league_df)
-    print(match_df)
-    print(player_df)
-    print(player_att_df)
-    print(team_df)
-    print(team_att_df)
+    X_train = X[:int(len(X) * 0.8)]
+    X_test = X[int((len(X) * 0.8)):]
+
+    y = pd.DataFrame()
+    y['match_result'] = np.where(X['home_team_goal'] > X['away_team_goal'], 1, 0)
+
+    y_train = y[:int(len(y) * 0.8)]
+    y_test = y[int(len(y) * 0.8):]
+
+    print(X.head(10))
+    print(y.head(10))
 
 
 if __name__ == "__main__":
