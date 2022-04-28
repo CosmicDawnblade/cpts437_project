0edb12ad71388e14437d30c7fc0335e32474efad
CosmicDawnblade <39527908+CosmicDawnblade@users.noreply.github.com>
2022-04-27 20:12:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
 
 
 def main():
@@ -27,6 +29,10 @@ def main():
     X = X.drop('date', axis=1)
     X = pd.merge(match_fil_df, X, left_on='home_team_api_id', right_on='team_api_id')
     X = X.drop(['home_team_api_id', 'team_api_id'], axis=1)
+    X['mean_rows'] = X.mean(axis=1)
+
+    for column in X:
+        X[column].fillna(X.mean_rows, inplace=True)
 
     X_train = X[:int(len(X) * 0.8)]
     X_test = X[int((len(X) * 0.8)):]
@@ -37,8 +43,19 @@ def main():
     y_train = y[:int(len(y) * 0.8)]
     y_test = y[int(len(y) * 0.8):]
 
-    print(X.head(10))
-    print(y.head(10))
+    clf = Perceptron()
+    # TRAINING
+    clf.fit(X_train, y_train.values.ravel())
+
+    # TESTING
+    y_pred = clf.predict(X_test)
+    score = accuracy_score(y_test, y_pred)
+    print("Testing accuracy", score)
+
+    # print(X.head(10))
+    # print(y.head(10))
+
+
 
 
 if __name__ == "__main__":
